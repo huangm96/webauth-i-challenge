dad981796e28933dccb721cd07a6a6efdea68f90
Min-Huang <minh.huang96@gmail.com>
2019-10-15 13:36:43 -0700

    added session to database

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require("express");
 const helmet = require("helmet");
 const sessions = require('express-session')
-
+const KnexSessionStore = require('connect-session-knex')(sessions);
 
 const server = express();
 const authRouter = require('./auth/auth-router.js')
+const knexConfig = require('./data/dbConfig.js')
 
 const sessionConfiguration = {
-    name: "jeep",
-    secret:" keep it secret, keep it safe!",
-    cookie: {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 5,
-        secure:false,
-    },
-    resave: false,
-    saveUninitialized: true,
-    
-}
+  name: "jeep",
+  secret: " keep it secret, keep it safe!",
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 5,
+    secure: false
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: knexConfig,
+    createtable: true, // automatically create the sessions table
+    clearIterval: 1000 * 60 * 30 // delete expired sessions every 30 mins
+  })
+};
 
 server.use(sessions(sessionConfiguration))
 server.use(helmet());
